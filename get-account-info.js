0f6f81e2cdc3b713f22232f1daa9c09096206956
john <johnathanchewy@gmail.com>
2019-10-02 22:19:41 -0700

    init commit

---
+++ get-account-info.js
@@ -0,0 +1,23 @@
+'use strict';
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+  server: 'wss://s.altnet.rippletest.net:51233' // TestNet Server
+});
+api.connect().then(() => {
+  /* begin custom code ------------------------------------ */
+  const myAddress = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
+
+  console.log('getting account info for', myAddress);
+  return api.getAccountInfo(myAddress);
+
+}).then(info => {
+  console.log(info);
+  console.log('getAccountInfo done');
+
+  /* end custom code -------------------------------------- */
+}).then(() => {
+  return api.disconnect();
+}).then(() => {
+  console.log('done and disconnected.');
+}).catch(console.error);
