f8719749ba7bdfd780ad7a39cf47c686cf06ba5c
john <johnathanchewy@gmail.com>
2019-10-02 23:44:08 -0700

    finished wallet creation function

---
+++ create-wallet.js
@@ -0,0 +1,12 @@
+const { Wallet, XRPAmount, XpringClient } = require("xpring-js");
+
+function generateWallet(privateKey) {
+    return Wallet.generateWalletFromSeed(privateKey);
+}
+
+async function testGenerateWallet() {
+    const wallet = generateWallet("snYP7oArxKepd3GPDcrjMsJYiJeJB");
+    console.log("wallet generated address: " + wallet.getAddress());
+}
+
+// testGenerateWallet();
